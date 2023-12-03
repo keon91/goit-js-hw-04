67bd1aab1883ee90d3bcfc2b47651512079e9535
[keon91] <maksym.o.vlasov@gmail.com>
2023-12-03 18:12:36 +0200

    test

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,27 @@
 'use strict'
+function isEnoughCapacity(products, containerSize) {
+    if (products.length <= containerSize) {
+        console.log(true);
+
+    } else {
+        console.log(false);
+        
+    }
+    return isEnoughCapacity;
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false

8f9c305071ceedb940d9ee5ba3ad5109983406c6
[keon91] <maksym.o.vlasov@gmail.com>
2023-12-05 18:27:32 +0200

    test

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,10 @@
 'use strict'
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
